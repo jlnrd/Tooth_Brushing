c4d64bd836071e5dc1d913a300a45f070548cd7a
Hunter Liu <hunterzhaoliu@gmail.com>
2019-06-03 13:03:32 -0400

    each subject has their own folder and all videos corresponding to subject is placed in that folder

--- rename_video_files_JAL.py
+++ rename_video_files_JAL.py
@@ -10,57 +10,95 @@ import csv
 # create an instance of the data in the csv
 # TODO: make it read in any csv with "Teeth brushing evening" in  the beginning?
 # Dr. Leonard's local = "/Volumes/Julia_HardDrive/Teeth brushing evening pilot v2 3.20.19_April 2, 2019_07.29.csv"
-# Hunter's local development = "/Users/appollo_liu/Downloads/Teeth brushing evening pilot FOR HUNTER.csv"
-teeth_brushing_csv = csv.DictReader(open("/Users/appollo_liu/Downloads/Teeth brushing evening pilot FOR HUNTER.csv"))
-
+# Hunter's local development = "/Users/appollo_liu/Documents/workspace/tooth_brushing_data/Teeth brushing evening pilot FOR HUNTER.csv"
+csv_path = "/Users/appollo_liu/Documents/workspace/tooth_brushing_data/Teeth brushing evening pilot FOR HUNTER.csv"
+teeth_brushing_csv = csv.DictReader(open(csv_path))
 # set the path to the videos
 # Dr. Leonard's local = "/Volumes/Julia_HardDrive/Q9/"
-# Hunter's local development = "/Users/appollo_liu/Downloads/"
-path_to_videos = "/Users/appollo_liu/Downloads/"
+# Hunter's local development = "/Users/appollo_liu/Documents/workspace/tooth_brushing_data/"
+path_to_videos = "/Users/appollo_liu/Documents/workspace/tooth_brushing_data/"
 
 # For loop through each row of csv
 for row in teeth_brushing_csv:
     # want to rename the video in the path_to_videos to: subject-month-date-year
-    recorded_date = row['Recorded Date']
+    recorded_date = row["Recorded Date"]
     # want to rename the video in the path_to_videos to :subject-month-date-year
-    subject = row['subject']
+    subject = row["subject"]
 
     # set the video file name that needs to be changed
-    current_file_name = row['ResponseId']
-    old_file_name = glob.glob(path_to_videos+current_file_name+'*.*')
+    current_file_name = row["ResponseId"]
+    old_file_name = glob.glob(path_to_videos + current_file_name + "*.*")
 
-    # recorded_date can be either 6 or 7 characters long depending on if the
-    # day in the year is greater than 9
-    if recorded_date[4] == "/":
-        # recorded_date day is greater than 9
-        if int(recorded_date[-5:-3]) < 12:
-            # submitted the day after the video was taken, so need to subtract one
-            # from the day
-            date_minus_1 = datetime(int(recorded_date[5:7])+2000, int(recorded_date[0]), int(recorded_date[2:4])) - timedelta(days=1)
-
-            # convert datetime date_minus_1 to string in format: subject-month-date-year
-            new_file_name = "subject"+subject+"-"+date_minus_1.strftime('%m-%-d-')[1:]+"19"+".mp4"
-        else:
-        	new_file_name ="subject"+subject+"-"+recorded_date.replace('/','-')[:7]+".mp4"
-    else:
-        # recorded_date day is less than or equal to 9
-        if int(recorded_date[-5:-3]) < 12:
-            # submitted the day after the video was taken, so need to subtract one
-            # from the day
-            date_minus_1 = datetime(int(recorded_date[5:7])+2000, int(recorded_date[0]), int(recorded_date[2])) - timedelta(days=1)
-
-            # convert datetime date_minus_1 to string in format: subject-month-date-year
-            new_file_name = "subject"+subject+"-"+date_minus_1.strftime('%m-%-d-')[1:]+"19"+".mp4"
-
-        else:
-        	new_file_name ="subject"+subject+"-"+recorded_date.replace('/','-')[:6]+".mp4"
-
-	# print os.path.join(path_to_videos, new_file_name)
-
-    # rename current_file_name to new_file_name
     if len(old_file_name) > 0 and os.path.isfile(old_file_name[0]):
-        print "old file found"
-    	# file is inside folder, so need to rename
-    	os.rename(old_file_name[0], path_to_videos+new_file_name)
+        # file is inside folder, so need to rename and put into subject folder
+        print("old file found")
+
+        # create subject folder if it doesn't already exist
+        if os.path.exists(path_to_videos + subject) == False:
+            # subject folder does not exist, need to create one
+            os.makedirs(path_to_videos + subject)
+
+        # recorded_date can be either 6 or 7 characters long depending on if the
+        # day in month is greater than 9
+
+        if recorded_date[4] == "/":
+            # recorded_date day is greater than 9
+            if int(recorded_date[-5:-3]) < 12:
+                # submitted the day after the video was taken, so need to subtract one
+                # from the day
+                date_minus_1 = datetime(
+                    int(recorded_date[5:7]) + 2000,
+                    int(recorded_date[0]),
+                    int(recorded_date[2:4]),
+                ) - timedelta(days=1)
+
+                # convert datetime date_minus_1 to string in format: subject-month-date-year
+                new_file_name = (
+                    "/subject"
+                    + subject
+                    + "-"
+                    + date_minus_1.strftime("%m-%-d-")[1:]
+                    + "19"
+                    + ".mp4"
+                )
+            else:
+                new_file_name = (
+                    "/subject"
+                    + subject
+                    + "-"
+                    + recorded_date.replace("/", "-")[:7]
+                    + ".mp4"
+                )
+        else:
+            # recorded_date day is less than or equal to 9
+            if int(recorded_date[-5:-3]) < 12:
+                # submitted the day after the video was taken, so need to subtract one
+                # from the day
+                date_minus_1 = datetime(
+                    int(recorded_date[5:7]) + 2000,
+                    int(recorded_date[0]),
+                    int(recorded_date[2]),
+                ) - timedelta(days=1)
+
+                # convert datetime date_minus_1 to string in format: subject-month-date-year
+                new_file_name = (
+                    "/subject"
+                    + subject
+                    + "-"
+                    + date_minus_1.strftime("%m-%-d-")[1:]
+                    + "19"
+                    + ".mp4"
+                )
+
+            else:
+                new_file_name = (
+                    "/subject"
+                    + subject
+                    + "-"
+                    + recorded_date.replace("/", "-")[:6]
+                    + ".mp4"
+                )
+
+        os.rename(old_file_name[0], path_to_videos + "/" + subject + new_file_name)
     else:
-       print "file doesn't exist"
+        print("file doesn't exist")
